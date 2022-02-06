44d2aaf25436bd23624a34cbecac1b2f0d7105de
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-02-06 06:30:52 +0000

    Added docstring

--- process.py
+++ process.py
@@ -1,7 +1,10 @@
 log_file = open("um-server-01.txt")
 
 
-def sales_reports(log_file):        # defines a new function
+def sales_reports(log_file):
+    """
+    Opens a file and prints the orders from Monday
+    """
     for line in log_file:           # reviews each line in the log
         line = line.rstrip()        # strips whitespace to the right of the word
         day = line[0:3]             # slices the first three letters of the day of the week
