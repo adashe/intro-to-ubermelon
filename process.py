eee4c9e840002f1a72a0eb7e7541287d947e0a80
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-02-06 03:40:17 +0000

    Changed sales report from Tuesday to Monday"
    clear
    quit()
    git status

---
+++ process.py
@@ -0,0 +1,12 @@
+log_file = open("um-server-01.txt")
+
+
+def sales_reports(log_file):        # defines a new function
+    for line in log_file:           # reviews each line in the log
+        line = line.rstrip()        # strips whitespace to the right of the word
+        day = line[0:3]             # slices the first three letters of the day of the week
+        if day == "Mon":            # finds Tuesday lines     # changed to Monday
+            print(line)             # prints Tuesday lines    # changed to Monday
+
+
+sales_reports(log_file)             # calls the function on the file
